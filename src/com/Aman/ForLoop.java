b866a47cf49f99e2247b9cf24affcb31672571dc
aman <caman411@gmail.com>
2021-09-24 07:35:50 +0530

    adding loop file

---
+++ src/com/Aman/ForLoop.java
@@ -0,0 +1,9 @@
+package com.Aman;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        for (int num = 1; num != 51; num++) {
+            System.out.println(num);
+        }
+    }
+}
---
+++ src/com/Aman/demo.java
@@ -0,0 +1,13 @@
+package com.Aman;
+
+import java.util.Scanner;
+
+public class demo {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int rollno = input.nextInt();
+        System.out.println("hey your rollno. is " + rollno);
+
+    }
+
+}
