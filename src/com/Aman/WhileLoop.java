8e7dd969f5f9acd4b66f3b145eddfad3c3f29d79
aman <caman411@gmail.com>
2021-09-24 07:35:32 +0530

    adding loop file

---
+++ src/com/Aman/WhileLoop.java
@@ -0,0 +1,11 @@
+package com.Aman;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        int num = 1;
+        while (num != 51){
+            System.out.println(num);
+            num++;
+        }
+    }
+}
